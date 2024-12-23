e558e3b4d3992de3861e7481c0fb5dfca652fbf1
kim-yu-rim-yjtech <yrkim@youngjintech.com>
2024-12-23 01:34:05 +0000

    Add all files to LFS

--- all_llm_pro/app/utils/ollama_serve_utils.py
+++ all_llm_pro/app/utils/ollama_serve_utils.py
@@ -1,11 +1,3 @@
-import os
-
-def start_ollama():
-    """Ollama 서비스를 시작"""
-    os.system("sudo systemctl start ollama")
-    print("Ollama 서비스를 시작했습니다.")
-
-def stop_ollama():
-    """Ollama 서비스를 중지"""
-    os.system("sudo systemctl stop ollama")
-    print("Ollama 서비스를 중지했습니다.")
+version https://git-lfs.github.com/spec/v1
+oid sha256:6877ff54c1a13ec72b46887108e797b2c230180323358b6d74070eeb107df771
+size 319
--- all_llm_pro/app/utils/stopwords.py
+++ all_llm_pro/app/utils/stopwords.py
@@ -1,13 +1,3 @@
-with open("stop_words.txt", "r", encoding="utf-8") as file:
-    STOP_WORDS = set(eval(file.read()))  
-def remove_stopwords(text):
-    """
-    텍스트에서 불용어를 제거하는 함수
-    """
-    text_lst = text.split()
-    
-    filtered_words = [word for word in text_lst if word not in STOP_WORDS]
-    
-    cleaned_text = ' '.join(filtered_words)
-    
-    return cleaned_text
+version https://git-lfs.github.com/spec/v1
+oid sha256:000ce92158dd615d0aea90602149534fc1915241b2f8724de5c443d2440c3198
+size 385
