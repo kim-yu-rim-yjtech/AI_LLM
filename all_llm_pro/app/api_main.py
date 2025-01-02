0398c10bd2633da71a71402a651261ce70b8dbd1
kim-yu-rim-yjtech <yrkim@youngjintech.com>
2025-01-02 14:42:06 +0900

    메인 파일 수정

--- all_llm_pro/app/api_main.py
+++ all_llm_pro/app/api_main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:beabe29c78e40859af977f09363ca9497307dabdf10a601d9d0300e0e771c007
-size 4415
+oid sha256:4317a31099a0cb6b0a196f9b26ae1170cd2c576db5aec6aaa2c0a17b12741ea8
+size 4444
